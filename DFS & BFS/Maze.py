50561c211397850812e1ac1be7709bce6cd11034
manavchauhan18 <61021578+manavchauhan18@users.noreply.github.com>
2020-05-26 11:43:52 +0530

    BFS AND DFS

---
+++ DFS & BFS/Maze.py
@@ -0,0 +1,203 @@
+import sys #The sys module provides information about constants, functions and methods of the Python interpreter.
+from logging import exception
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(Node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise exception("Empty Frontier")
+
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise exception("Empty Frontier")
+
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
+class Maze():
+
+    def _init__(self, filename):
+
+        with open(filename) as f:
+            contents = f.read()
+
+        if contents.count("A") != 1:
+            raise exception("Maze must have exactly one start point")
+        if contents.count("B") != 1:
+            raise exception("Maze must have exactly one goal")
+
+        contents = contents.splitlines() #Splits the string based on the lines.
+        self.height = len(contents) #Length of the contents = height
+        self.width = max(len(line) for line in contents) #Max length of line in the contents = width
+
+        self.walls = []
+        for i in range(self.width):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents[i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "B":
+                        self.goal = (i, j)
+                        row .append(False)
+                    elif contents[i][j] == " ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+
+        self.solution = None
+
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        print()
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+                if col:
+                    print("█", end = "")
+                elif (i, j) == self.start:
+                    print("A", end = "")
+                elif (i, j) == self.goal:
+                    print("B", end = "")
+                elif solution is not None and (i, j) in solution:
+                    print("*", end = "")
+                else:
+                    print(" ", end = "")
+            print()
+        print()
+
+    def neighbors(self, state):
+        row, col = state
+        candidates = [
+            ("up", (row - 1, col)),
+            ("down", (row + 1, col)),
+            ("left", (row, col - 1)),
+            ("right", (row, col + 1))
+        ]
+
+        result = []
+        for action, (r,c) in candidates:
+            if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
+                result.append((action, (r,c)))
+        return result
+
+    def solve(self):
+
+        self.num_explored = 0
+
+        start = Node(state=self.start, parent=None, action=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+
+        self.explored = set()
+
+        while True:
+
+            if frontier.empty():
+                raise exception("No Solution")
+
+            node = frontier.remove()
+            self.num_explored += 1
+
+            if node.state == self.goal:
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            self.explored.add(node.state)
+
+            for actions, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+    def output_image(self, filename, show_solution=True, show_explored=False):
+        from PIL import Image, ImageDraw
+        cell_size = 50
+        cell_border = 2
+
+        #Create a black canvas
+        img = Image.new(
+            "RGBA : black",
+            (self.width * cell_size, self.height * cell_size)
+
+        )
+        draw = ImageDraw.Draw(img)
+
+        solution = self.solution[1] if self.solution is not None else None
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+
+                if col:
+                    fill = (40, 40, 40)
+
+                elif (i, j) == self.start:
+                    fill = (255, 0, 0)
+
+                elif (i, j) == self.goal:
+                    fill = (0, 171, 28)
+
+                #Solution
+                elif solution is not None and show_solution and (i, j) in solution:
+                    fill = (220, 235, 113)
+
+                #Explored
+                elif solution is not None and show_solution and (i, j) in self.explored:
+                    fill =  (212, 97, 85)
+
+                else:
+                    fill = (237, 240, 252)
+
+                draw.rectangle(
+                    ([(j * cell_size + border, i * cell_size + cell_border),
+                        ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
+                    fill=fill
+                )
+
+        img.save(filename)
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python maze.py maze.txt")
+
+m = Maze(sys.argv[1])
+print("Maze: ")
+print("Solving...")
+print("States Explored:", m.num_explored)
+print("Solution:")
+m.output_image("maze.png", show_explored=True)
